aec6e831005fdd85de9708a888416a69b59d2580
niktob560 <niktob560@gmail.com>
2020-04-19 14:29:15 +0300

    Fixed typing, added more warnings

--- Inc/graphbases.hpp
+++ Inc/graphbases.hpp
@@ -1,10 +1,11 @@
 #pragma once
 #include <unistd.h>
+#include <cstdint>
 #include "math.hpp"
 
 namespace graphbases
 {
-    typedef float COORDS_DATATYPE;
+    typedef int16_t COORDS_DATATYPE;
     
 	struct array
 	{
@@ -18,14 +19,39 @@ namespace graphbases
 		COORDS_DATATYPE     x,
 							y;
 
-		bool eq(struct coords *c)
+		constexpr bool eq(const struct coords *c)
 		{
 			return (x == c->x && y == c->y);
 		}
+		constexpr bool eq(const struct coords c)
+		{
+			return (x == c.x && y == c.y);
+		}
 
-		bool eq(COORDS_DATATYPE _x, COORDS_DATATYPE _y)
+		constexpr bool eq(const COORDS_DATATYPE _x, const COORDS_DATATYPE _y)
 		{
 			return (x == _x && y == _y);
+		}
+
+
+		constexpr struct coords rotate(const float teta)
+		{
+			return {static_cast<COORDS_DATATYPE>(x * cos(teta) - y * sin(teta)), static_cast<COORDS_DATATYPE>(x * sin(teta) + y * cos(teta))};
+		}
+
+		constexpr struct coords moveToNewZero(const struct coords *newZero)
+		{
+			return {static_cast<COORDS_DATATYPE>(newZero->x - x), static_cast<COORDS_DATATYPE>(newZero->y - y)};
+		}
+
+		constexpr struct coords 	restoreGlobalZero(const struct coords *newZero)
+		{
+			return {static_cast<COORDS_DATATYPE>(newZero->x + x), static_cast<COORDS_DATATYPE>(newZero->y + y)};
+		}
+
+		constexpr struct coords rotate(const struct coords *zero, const float teta)
+		{
+			return moveToNewZero(zero).rotate(teta).restoreGlobalZero(zero);
 		}
 	};
 
@@ -34,11 +60,10 @@ namespace graphbases
     struct graphPoint
     {
         struct graphPoint **targets = NULL;
-        size_t numOfTargets = 0;
-        struct coords c;
-        COORDS_DATATYPE weight = INF;
-        size_t i = 0;
-        bool calculated = false;
+        size_t 				numOfTargets = 0;
+        uint16_t		 	weight = 0xFFFF;
+        size_t 				i = 0;
+        bool 				calculated = false;
     };
     
 }
--- Src/main.cpp
+++ Src/main.cpp
@@ -4,6 +4,5 @@
 int main()
 {
     struct graphbases::graphPoint p0;
-    p0.c = {1, 1};
     return 0;
 }
